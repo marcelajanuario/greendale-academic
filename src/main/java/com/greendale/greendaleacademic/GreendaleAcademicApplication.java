bc66f8e148c8dfab34c94400f6f37fc67011a48a
Marcela Januario <marcelajanuario92@hotmail.com>
2023-11-13 19:41:25 -0300

    Adiciona estrutura do projeto

---
+++ src/main/java/com/greendale/greendaleacademic/GreendaleAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.greendale.greendaleacademic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreendaleAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreendaleAcademicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greendale/greendaleacademic/GreendaleAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greendale.greendaleacademic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreendaleAcademicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
